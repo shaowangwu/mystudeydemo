cb02857c1247df04115832d6b4171f83bf3454a9
shaowangwu <15918865050@163.com>
2022-03-04 22:22:21 +0800

    线程通信之生产者消费者阻塞队列

--- learnProjectDemo/src/main/java/juc/demo/ProdConsumer_BlockingQueueDemo.java
+++ learnProjectDemo/src/main/java/juc/demo/ProdConsumer_BlockingQueueDemo.java
@@ -1,9 +1,14 @@
 package juc.demo;
 
+class MyResource{
+    private boolean flag=true;
+
+}
 /**
 * @author:shaowangwu
 * @Date: 2022/3/4 15:40
-* Description:
+* Description:线程通信之生产者消费者阻塞队列
+ * volatile/CAS/atomicInteger/BlockingQueue/线程交互/原子引用
 */
 public class ProdConsumer_BlockingQueueDemo {
 
