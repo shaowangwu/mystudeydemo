2ba9deb9cb2a98e3a40044ec8c40488d9d9a9b2c
shaowangwu <15918865050@163.com>
2022-03-04 14:33:51 +0800

    add divide fuction

---
+++ learnProjectDemo/src/main/java/com/atguigu/averageAssignDemo.java
@@ -0,0 +1,45 @@
+package com.atguigu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class averageAssignDemo {
+
+    public static <T>List<List<T>> averageDividelist(List<T> source,int n){
+        List<List<T>> lists = new ArrayList<List<T>>();
+        //取余数
+        int remainder = source.size()%n;
+        //取商
+        int number = source.size()/n;
+        //偏移量
+        int offset=0;
+
+        for (int i = 0; i < n; i++) {
+            List<T> val=null;
+           if(remainder>0){
+               val=source.subList(i*number+offset,(i+1)*number+offset+1);
+               remainder--;
+               offset++;
+           }else{
+               val=source.subList(i*number+offset,(i+1)*number+offset);
+           }
+
+           if(val!=null && val.size()>0){
+               lists.add(val);
+           }
+        }
+
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("a");
+        list.add("111");
+        list.add("bbb");
+
+        averageDividelist(list,5);
+    }
+
+
+}
