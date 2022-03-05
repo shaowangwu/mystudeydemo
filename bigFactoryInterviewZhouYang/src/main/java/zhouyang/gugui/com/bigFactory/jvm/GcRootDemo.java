072e4a610b437ceaf6b412010a15420be2e0df16
shaowangwu <15918865050@163.com>
2022-03-05 20:11:19 +0800

    sout

---
+++ bigFactoryInterviewZhouYang/src/main/java/zhouyang/gugui/com/bigFactory/jvm/GcRootDemo.java
@@ -0,0 +1,15 @@
+package zhouyang.gugui.com.bigFactory.jvm;
+/**
+* @author:shaowangwu
+* @Date: 2022/3/5 20:05
+* Description:
+*/
+public class GcRootDemo {
+
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
+
+
+
+}
